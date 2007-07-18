fc5fe0baf94596acbde604ce2221a20ec7d7cb88
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2007-07-18 14:32:41 +0000

    Einstellungen für Trennzeichen, Dezimaltrennzeichen und Anzahl Kopfzeilen werden in einem Dialog abgefragt.

---
+++ trunk/TxtEinstellungen.Designer.vb
@@ -0,0 +1,166 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TxtEinstellungen
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button_Cancel = New System.Windows.Forms.Button
+        Me.Button_OK = New System.Windows.Forms.Button
+        Me.Label_Trennzeichen = New System.Windows.Forms.Label
+        Me.ComboBox_Trennzeichen = New System.Windows.Forms.ComboBox
+        Me.Label_Dezimaltrennzeichen = New System.Windows.Forms.Label
+        Me.ComboBox_Dezimaltrennzeichen = New System.Windows.Forms.ComboBox
+        Me.Label_AnzKopfzeilen = New System.Windows.Forms.Label
+        Me.TextBox_AnzKopfzeilen = New System.Windows.Forms.MaskedTextBox
+        Me.RichTextBox_Vorschau = New System.Windows.Forms.RichTextBox
+        Me.Label_Vorschau = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button_Cancel
+        '
+        Me.Button_Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button_Cancel.Location = New System.Drawing.Point(368, 321)
+        Me.Button_Cancel.Name = "Button_Cancel"
+        Me.Button_Cancel.Size = New System.Drawing.Size(75, 23)
+        Me.Button_Cancel.TabIndex = 0
+        Me.Button_Cancel.Text = "Abbrechen"
+        Me.Button_Cancel.UseVisualStyleBackColor = True
+        '
+        'Button_OK
+        '
+        Me.Button_OK.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Button_OK.Location = New System.Drawing.Point(287, 320)
+        Me.Button_OK.Name = "Button_OK"
+        Me.Button_OK.Size = New System.Drawing.Size(75, 23)
+        Me.Button_OK.TabIndex = 1
+        Me.Button_OK.Text = "OK"
+        Me.Button_OK.UseVisualStyleBackColor = True
+        '
+        'Label_Trennzeichen
+        '
+        Me.Label_Trennzeichen.AutoSize = True
+        Me.Label_Trennzeichen.Location = New System.Drawing.Point(13, 13)
+        Me.Label_Trennzeichen.Name = "Label_Trennzeichen"
+        Me.Label_Trennzeichen.Size = New System.Drawing.Size(75, 13)
+        Me.Label_Trennzeichen.TabIndex = 2
+        Me.Label_Trennzeichen.Text = "Trennzeichen:"
+        '
+        'ComboBox_Trennzeichen
+        '
+        Me.ComboBox_Trennzeichen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox_Trennzeichen.DropDownWidth = 100
+        Me.ComboBox_Trennzeichen.FormattingEnabled = True
+        Me.ComboBox_Trennzeichen.Location = New System.Drawing.Point(116, 10)
+        Me.ComboBox_Trennzeichen.Name = "ComboBox_Trennzeichen"
+        Me.ComboBox_Trennzeichen.Size = New System.Drawing.Size(100, 21)
+        Me.ComboBox_Trennzeichen.TabIndex = 3
+        '
+        'Label_Dezimaltrennzeichen
+        '
+        Me.Label_Dezimaltrennzeichen.AutoSize = True
+        Me.Label_Dezimaltrennzeichen.Location = New System.Drawing.Point(232, 13)
+        Me.Label_Dezimaltrennzeichen.Name = "Label_Dezimaltrennzeichen"
+        Me.Label_Dezimaltrennzeichen.Size = New System.Drawing.Size(108, 13)
+        Me.Label_Dezimaltrennzeichen.TabIndex = 4
+        Me.Label_Dezimaltrennzeichen.Text = "Dezimaltrennzeichen:"
+        '
+        'ComboBox_Dezimaltrennzeichen
+        '
+        Me.ComboBox_Dezimaltrennzeichen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox_Dezimaltrennzeichen.DropDownWidth = 50
+        Me.ComboBox_Dezimaltrennzeichen.FormattingEnabled = True
+        Me.ComboBox_Dezimaltrennzeichen.Items.AddRange(New Object() {".", ","})
+        Me.ComboBox_Dezimaltrennzeichen.Location = New System.Drawing.Point(349, 10)
+        Me.ComboBox_Dezimaltrennzeichen.Name = "ComboBox_Dezimaltrennzeichen"
+        Me.ComboBox_Dezimaltrennzeichen.Size = New System.Drawing.Size(50, 21)
+        Me.ComboBox_Dezimaltrennzeichen.TabIndex = 5
+        '
+        'Label_AnzKopfzeilen
+        '
+        Me.Label_AnzKopfzeilen.AutoSize = True
+        Me.Label_AnzKopfzeilen.Location = New System.Drawing.Point(13, 47)
+        Me.Label_AnzKopfzeilen.Name = "Label_AnzKopfzeilen"
+        Me.Label_AnzKopfzeilen.Size = New System.Drawing.Size(94, 13)
+        Me.Label_AnzKopfzeilen.TabIndex = 6
+        Me.Label_AnzKopfzeilen.Text = "Anzahl Kopfzeilen:"
+        '
+        'TextBox_AnzKopfzeilen
+        '
+        Me.TextBox_AnzKopfzeilen.Location = New System.Drawing.Point(116, 44)
+        Me.TextBox_AnzKopfzeilen.Mask = "0#"
+        Me.TextBox_AnzKopfzeilen.Name = "TextBox_AnzKopfzeilen"
+        Me.TextBox_AnzKopfzeilen.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.TextBox_AnzKopfzeilen.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox_AnzKopfzeilen.TabIndex = 7
+        '
+        'RichTextBox_Vorschau
+        '
+        Me.RichTextBox_Vorschau.BackColor = System.Drawing.Color.White
+        Me.RichTextBox_Vorschau.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox_Vorschau.Location = New System.Drawing.Point(16, 112)
+        Me.RichTextBox_Vorschau.Name = "RichTextBox_Vorschau"
+        Me.RichTextBox_Vorschau.ReadOnly = True
+        Me.RichTextBox_Vorschau.Size = New System.Drawing.Size(230, 181)
+        Me.RichTextBox_Vorschau.TabIndex = 8
+        Me.RichTextBox_Vorschau.Text = ""
+        Me.RichTextBox_Vorschau.WordWrap = False
+        '
+        'Label_Vorschau
+        '
+        Me.Label_Vorschau.AutoSize = True
+        Me.Label_Vorschau.Location = New System.Drawing.Point(13, 86)
+        Me.Label_Vorschau.Name = "Label_Vorschau"
+        Me.Label_Vorschau.Size = New System.Drawing.Size(55, 13)
+        Me.Label_Vorschau.TabIndex = 9
+        Me.Label_Vorschau.Text = "Vorschau:"
+        '
+        'TxtEinstellungen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 356)
+        Me.Controls.Add(Me.Label_Vorschau)
+        Me.Controls.Add(Me.RichTextBox_Vorschau)
+        Me.Controls.Add(Me.TextBox_AnzKopfzeilen)
+        Me.Controls.Add(Me.Label_AnzKopfzeilen)
+        Me.Controls.Add(Me.ComboBox_Dezimaltrennzeichen)
+        Me.Controls.Add(Me.Label_Dezimaltrennzeichen)
+        Me.Controls.Add(Me.ComboBox_Trennzeichen)
+        Me.Controls.Add(Me.Label_Trennzeichen)
+        Me.Controls.Add(Me.Button_OK)
+        Me.Controls.Add(Me.Button_Cancel)
+        Me.Name = "TxtEinstellungen"
+        Me.Text = "Einstellungen"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button_Cancel As System.Windows.Forms.Button
+    Friend WithEvents Button_OK As System.Windows.Forms.Button
+    Friend WithEvents Label_Trennzeichen As System.Windows.Forms.Label
+    Friend WithEvents ComboBox_Trennzeichen As System.Windows.Forms.ComboBox
+    Friend WithEvents Label_Dezimaltrennzeichen As System.Windows.Forms.Label
+    Friend WithEvents ComboBox_Dezimaltrennzeichen As System.Windows.Forms.ComboBox
+    Friend WithEvents Label_AnzKopfzeilen As System.Windows.Forms.Label
+    Friend WithEvents TextBox_AnzKopfzeilen As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents RichTextBox_Vorschau As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label_Vorschau As System.Windows.Forms.Label
+End Class
---
+++ trunk/TxtEinstellungen.vb
@@ -0,0 +1,173 @@
+Public Class TxtEinstellungen
+
+#Region "Eigenschaften"
+
+    'Eigenschaften
+    '#############
+    Private _file As String                     'Pfad zur Datei
+    Private _trennzeichen As zeichen            'Spaltentrennzeichen
+    Private _dezimaltrennzeichen As zeichen     'Dezimaltrennzeichen
+    Private _anzKopfzeilen As Integer           'Anzahl Kopfzeilen
+
+    Private semikolon As zeichen = New zeichen(";")
+    Private komma As zeichen = New zeichen(",")
+    Private punkt As zeichen = New zeichen(".")
+    private leerzeichen As zeichen = New zeichen(" ")
+    private tab As zeichen = New zeichen(Chr(09))
+
+#End Region 'Eigenschaften
+
+#Region "Properties"
+
+    'Properties
+    '##########
+    Public Property File() As String
+        Get
+            Return _file
+        End Get
+        Set(ByVal value As String)
+            _file = value
+        End Set
+    End Property
+
+    Public Property Trennzeichen() As zeichen
+        Get
+            Return _trennzeichen
+        End Get
+        Set(ByVal value As zeichen)
+            _trennzeichen = value
+            Me.ComboBox_Trennzeichen.SelectedItem = _trennzeichen
+        End Set
+    End Property
+
+    Public Property Dezimaltrennzeichen() As zeichen
+        Get
+            Return _dezimaltrennzeichen
+        End Get
+        Set(ByVal value As zeichen)
+            _dezimaltrennzeichen = value
+            Me.ComboBox_Dezimaltrennzeichen.SelectedItem = _dezimaltrennzeichen
+        End Set
+    End Property
+
+    Public Property AnzKopfzeilen() As Integer
+        Get
+            Return _anzKopfzeilen
+        End Get
+        Set(ByVal value As Integer)
+            _anzKopfzeilen = value
+            Me.TextBox_AnzKopfzeilen.Text = _anzKopfzeilen.ToString()
+        End Set
+    End Property
+
+#End Region 'Properties
+
+#Region "Methoden"
+
+    'Methoden
+    '########
+
+    'Form laden
+    '**********
+    Private Sub TextSourceSettings_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        'Combobox Trennzeichen initialisieren
+        Me.ComboBox_Trennzeichen.BeginUpdate()
+        Me.ComboBox_Trennzeichen.Items.Add(Me.semikolon)
+        Me.ComboBox_Trennzeichen.Items.Add(Me.komma)
+        Me.ComboBox_Trennzeichen.Items.Add(Me.punkt)
+        Me.ComboBox_Trennzeichen.Items.Add(Me.leerzeichen)
+        Me.ComboBox_Trennzeichen.Items.Add(Me.tab)
+        Me.ComboBox_Trennzeichen.EndUpdate()
+
+        'Combobox Dezimaltrennzeichen initialisieren
+        Me.ComboBox_Dezimaltrennzeichen.BeginUpdate()
+        Me.ComboBox_Dezimaltrennzeichen.Items.Add(Me.komma)
+        Me.ComboBox_Dezimaltrennzeichen.Items.Add(Me.punkt)
+        Me.ComboBox_Dezimaltrennzeichen.EndUpdate()
+
+        'Standard-Trennzeichen setzen
+        Me.Trennzeichen = Me.semikolon
+
+        'Standard-Dezimaltrennzeichen setzen
+        Me.Dezimaltrennzeichen = Me.punkt
+
+        'Standard-Anzahl Kopfzeilen setzen
+        Me.AnzKopfzeilen = 1
+
+        'Datei als Vorschau anzeigen
+        Me.RichTextBox_Vorschau.LoadFile(Me.File, RichTextBoxStreamType.PlainText)
+
+    End Sub
+
+    'OK Button gedrückt
+    '******************
+    Private Sub Button_OK_Click( ByVal sender As System.Object,  ByVal e As System.EventArgs) Handles Button_OK.Click
+
+        'Einstellungen übernehmen
+        '------------------------
+
+        'Trenn- und Dezimaltrennzeichen
+        Me.Trennzeichen = Me.ComboBox_Trennzeichen.SelectedItem
+        Me.Dezimaltrennzeichen = Me.ComboBox_Dezimaltrennzeichen.SelectedItem
+        
+        'Anzahl Kopfzeilen
+        Me.TextBox_AnzKopfzeilen.ValidatingType = GetType(System.Int32)
+        If (Me.TextBox_AnzKopfzeilen.ValidateText() = Nothing) Then
+            MsgBox("Bitte eine Zahl für die Anzahl der Kopfzeilen eingeben!", MsgBoxStyle.Exclamation, "Fehler")
+            Me.TextBox_AnzKopfzeilen.Focus()
+            Me.DialogResult = Windows.Forms.DialogResult.None
+            Exit Sub
+        Else
+            Me.AnzKopfzeilen = Convert.ToInt32(TextBox_AnzKopfzeilen.Text)
+        End If
+
+    End Sub
+
+#End Region 'Methoden
+
+End Class
+
+Public Class zeichen
+
+    Private _char As Char
+
+    Public Property Character() As Char
+        Get
+            Return _char
+        End Get
+        Set(ByVal value As Char)
+            _char = value
+        End Set
+    End Property
+
+    'Konstruktoren
+    '*************
+    Public Sub New(ByVal str As String)
+        Character = Convert.ToChar(str)
+    End Sub
+
+    Public Sub New(ByVal chr As Char)
+        Character = chr
+    End Sub
+
+    'Zeichenausgabe als String
+    '*************************
+    Public Overrides Function tostring() As String
+
+        Dim output As String
+
+        Select Case Character
+            Case Chr(32)
+                output = "Leerzeichen"
+            Case Chr(9)
+                output = "Tab"
+            Case Else
+                output = Convert.ToString(Character)
+        End Select
+
+        Return output
+
+    End Function
+
+End Class
