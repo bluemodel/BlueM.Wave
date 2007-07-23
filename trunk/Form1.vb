c8f71b9bd0dc6d222ac6f5e514049a78807dfca7
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2007-07-23 09:27:21 +0000

    Import-Funktionen ausgelagert und von aussen zugaenglich gemacht (Bug 136)

--- trunk/Form1.vb
+++ trunk/Form1.vb
@@ -10,90 +10,24 @@ Public Class Zeitreihendarstellung
 
     'Textdatei als Zeitreihe importieren
     '***********************************
-    Private Sub Zeitreihenimport(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_TxtImport.Click
+    Private Sub Import_File(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_TxtImport.Click
 
         Dim DialogResult As DialogResult
-        Dim i As Integer
 
         'Datei-öffnen Dialog anzeigen
         DialogResult = Me.OpenFileDialog1.ShowDialog()
         If (DialogResult = Windows.Forms.DialogResult.OK) Then
 
-            Select case Path.GetExtension(Me.OpenFileDialog1.FileName).ToUpper()
+            Select Case Path.GetExtension(Me.OpenFileDialog1.FileName).ToUpper()
 
                 Case ".ZRE"
-                    'ZRE-Objekt intialisieren
-                    Dim ZRE as New ZRE(Me.OpenFileDialog1.FileName)
-                    'Serie zeichnen
-                    '--------------
-                    Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
-                    'Namen vergeben
-                    Line1.Title = ZRE.Zeitreihe.Title
-                    'X-Werte als Zeitdaten einstellen
-                    Line1.XValues.DateTime = True
-                    'Anzahl Serien bestimmen
-                    Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
-                    'Neue Serie hinzufügen
-                    Me.TChart1.Chart.Series(AnzahlSerien - 1).Add(ZRE.Zeitreihe.XWerte, ZRE.Zeitreihe.YWerte)
+                    Import_ZRE(Me.OpenFileDialog1.FileName)
 
                 Case ".WEL"
-                    'WEL-Dialog initialisieren
-                    Dim WEL As New WEL()
-                    WEL.File = Me.OpenFileDialog1.FileName
-
-                    DialogResult = WEL.ShowDialog()
-                    If (DialogResult = Windows.Forms.DialogResult.OK) Then
-                        For i = 0 To WEL.Zeitreihen.GetUpperBound(0)
-                            'Serie zeichnen
-                            '--------------
-                            Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
-                            'X-Werte als Zeitdaten einstellen
-                            Line1.XValues.DateTime = True
-                            'Namen vergeben
-                            Line1.Title = WEL.Zeitreihen(i).Title
-                            'Anzahl Serien bestimmen
-                            Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
-                            'Neue Serie hinzufügen
-                            Me.TChart1.Chart.Series(AnzahlSerien - 1).Add(WEL.Zeitreihen(i).XWerte, WEL.Zeitreihen(i).YWerte)
-                        Next
-                    End If
-
+                    Import_WEL(Me.OpenFileDialog1.FileName)
 
                 Case Else
-                    'TXT-Dialog initialisieren
-                    Dim TXT As New TXT()
-                    TXT.File = Me.OpenFileDialog1.FileName
-
-                    'Einstellungen-Dialog anzeigen
-                    DialogResult = TXT.ShowDialog()
-                    If (DialogResult = Windows.Forms.DialogResult.OK) Then
-
-                        Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
-                        'Namen vergeben
-                        Line1.Title = Path.GetFileNameWithoutExtension(Me.OpenFileDialog1.FileName)
-                        'X-Werte als Zeitdaten einstellen
-                        Line1.XValues.DateTime = True
-
-                        'An TextSource übergeben
-                        '-----------------------
-                        Me.TextSource1.Series = Line1
-                        'Trennzeichen einstellen
-                        Me.TextSource1.Separator = TXT.Trennzeichen.Character
-                        'Dezimaltrennzeichen einstellen
-                        Me.TextSource1.DecimalSeparator = TXT.Dezimaltrennzeichen.Character
-                        'Anzahl Kopfzeilen einstellen
-                        Me.TextSource1.HeaderLines = TXT.AnzKopfzeilen
-                        'Datei einlesen
-                        Me.TextSource1.LoadFromFile(TXT.File)
-
-                        'Serie zeichnen
-                        '--------------
-                        'Anzahl Serien bestimmen
-                        Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
-                        'Neue Serie hinzufügen
-                        Me.TChart1.Chart.Series(AnzahlSerien - 1).DataSource = Me.TextSource1.Series
-
-                    End If
+                    Import_TXT(Me.OpenFileDialog1.FileName)
 
             End Select
 
@@ -101,9 +35,96 @@ Public Class Zeitreihendarstellung
 
     End Sub
 
+    'ZRE-Datei importieren
+    '*********************
+    Public Sub Import_ZRE(ByVal FileName As String)
+
+        'ZRE-Objekt intialisieren
+        Dim ZRE As New ZRE(FileName)
+        'Serie zeichnen
+        '--------------
+        Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
+        'Namen vergeben
+        Line1.Title = ZRE.Zeitreihe.Title
+        'X-Werte als Zeitdaten einstellen
+        Line1.XValues.DateTime = True
+        'Anzahl Serien bestimmen
+        Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
+        'Neue Serie hinzufügen
+        Me.TChart1.Chart.Series(AnzahlSerien - 1).Add(ZRE.Zeitreihe.XWerte, ZRE.Zeitreihe.YWerte)
+
+    End Sub
+
+    'WEL-Datei importieren
+    '*********************
+    Public Sub Import_WEL(ByVal FileName As String)
+        
+        Dim i as Integer
+        
+        'WEL-Dialog initialisieren
+        Dim WEL As New WEL()
+        WEL.File = FileName
+
+        If (WEL.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+            For i = 0 To WEL.Zeitreihen.GetUpperBound(0)
+                'Serie zeichnen
+                '--------------
+                Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
+                'X-Werte als Zeitdaten einstellen
+                Line1.XValues.DateTime = True
+                'Namen vergeben
+                Line1.Title = WEL.Zeitreihen(i).Title
+                'Anzahl Serien bestimmen
+                Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
+                'Neue Serie hinzufügen
+                Me.TChart1.Chart.Series(AnzahlSerien - 1).Add(WEL.Zeitreihen(i).XWerte, WEL.Zeitreihen(i).YWerte)
+            Next
+        End If
+
+    End Sub
+
+    'TXT-Datei importieren
+    '*********************
+    Public Sub Import_TXT(FileName as String)
+
+        'TXT-Dialog initialisieren
+        Dim TXT As New TXT()
+        TXT.File = FileName
+
+        'Einstellungen-Dialog anzeigen
+        If (TXT.ShowDialog() = Windows.Forms.DialogResult.OK) Then
+
+            Dim Line1 As New Steema.TeeChart.Styles.Line(Me.TChart1.Chart)
+            'Namen vergeben
+            Line1.Title = Path.GetFileNameWithoutExtension(TXT.File)
+            'X-Werte als Zeitdaten einstellen
+            Line1.XValues.DateTime = True
+
+            'An TextSource übergeben
+            '-----------------------
+            Me.TextSource1.Series = Line1
+            'Trennzeichen einstellen
+            Me.TextSource1.Separator = TXT.Trennzeichen.Character
+            'Dezimaltrennzeichen einstellen
+            Me.TextSource1.DecimalSeparator = TXT.Dezimaltrennzeichen.Character
+            'Anzahl Kopfzeilen einstellen
+            Me.TextSource1.HeaderLines = TXT.AnzKopfzeilen
+            'Datei einlesen
+            Me.TextSource1.LoadFromFile(TXT.File)
+
+            'Serie zeichnen
+            '--------------
+            'Anzahl Serien bestimmen
+            Dim AnzahlSerien As Integer = Me.TChart1.Series.Count
+            'Neue Serie hinzufügen
+            Me.TChart1.Chart.Series(AnzahlSerien - 1).DataSource = Me.TextSource1.Series
+
+        End If
+    End Sub
+
     'TEN-Datei importieren
     '*********************
-    Private Sub TENImport(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_TENImport.Click
+    Public Sub Import_TEN(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_TENImport.Click
         TChart1.Import.ShowImportDialog()
     End Sub
 
