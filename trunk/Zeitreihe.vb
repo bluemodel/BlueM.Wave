81bba076d9e66805fac9f4a44580a1c26cebf75c
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2007-07-24 15:39:32 +0000

    Anbindung an EVO Bug 136

--- trunk/Zeitreihe.vb
+++ trunk/Zeitreihe.vb
@@ -50,8 +50,8 @@ Public Class Zeitreihe
         End Get
         Set(ByVal value As Integer)
             _length = value
-            ReDim Me.XWerte(Me.Length - 1)
-            ReDim Me.YWerte(Me.Length - 1)
+            ReDim Me.XWerte(_length - 1)
+            ReDim Me.YWerte(_length - 1)
         End Set
     End Property
 
@@ -63,6 +63,70 @@ Public Class Zeitreihe
     '***********
     Public Sub New(ByVal title As String)
         Me.Title = title
+    End Sub
+
+    'Public Shared Operator =(ByVal zre1 As Zeitreihe, ByVal zre2 As Zeitreihe) As Zeitreihe
+    '    zre1.Length = zre2.Length
+    '    zre1.Title = zre2.Title
+    '    zre1.XWerte = zre2.XWerte
+    '    zre1.YWerte = zre2.YWerte
+    '    return zre1
+    'End Operator
+
+    'Zeitreihe kopieren
+    '******************
+    Public Function copy() As Zeitreihe
+        Dim target As New Zeitreihe(Me.Title)
+        target.Length = Me.Length
+        target.XWerte = Me.XWerte
+        target.YWerte = Me.YWerte
+        Return target
+    End Function
+
+    'Zeitreihe kürzen
+    '****************
+    Public Sub cut(ByVal start As DateTime, ByVal ende As DateTime)
+
+        Dim j, k As Integer
+
+        'Zeitreihe kopieren
+        Dim tmpZeitreihe As New Zeitreihe("tmp")
+        tmpZeitreihe = Me.copy()
+
+        'Zeitschrittweite feststellen
+        'BUG 105: Es wird von konstanten Zeitschritten ausgegangen
+        Dim dt As TimeSpan = Me.XWerte(1) - Me.XWerte(0)
+
+        'Wenn dt >= 1 Tag, Start und Ende auf Tage runden
+        If (dt.TotalDays >= 1) Then
+            start = start.Date
+            ende = ende.Date
+        End If
+
+        'Neue Länge der Zeitreihe
+        Dim length As Integer = ((ende - start).TotalSeconds / dt.TotalSeconds) + 1
+
+        'Zeitreihe redimensionieren
+        Me.Length = length
+
+        'bis Start vorspulen
+        j = 0
+        Do Until (tmpZeitreihe.XWerte(j) >= start)
+            j += 1
+        Loop
+
+        'bis Ende Werte in Zeitreihe zurückkopieren
+        k = 0
+        Do Until (tmpZeitreihe.XWerte(j) > ende)
+            Me.XWerte(k) = tmpZeitreihe.XWerte(j)
+            Me.YWerte(k) = tmpZeitreihe.YWerte(j)
+            j += 1
+            k += 1
+            If (j > tmpZeitreihe.XWerte.GetUpperBound(0)) Then
+                Exit Do
+            End If
+        Loop
+
     End Sub
 
 #End Region 'Methoden
