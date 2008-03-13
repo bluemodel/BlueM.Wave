3e5be687b423463f0ca56668f29b88fa7678b7eb
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2008-03-13 09:47:56 +0000

    Spalten werden jetzt bereits im Konstruktor ausgelesen -> besseres Verhalten von getReihe()

--- trunk/Dateiformate/Dateiformat.vb
+++ trunk/Dateiformate/Dateiformat.vb
@@ -10,6 +10,9 @@ Public MustInherit Class Dateiformat
     Public leerzeichen As Zeichen = New Zeichen(" ")
     Public tab As Zeichen = New Zeichen(Chr(9))
 
+    'echte Konstanten
+    Protected Const eol As String = Chr(13) & Chr(10)   'Zeilenumbruch
+
 #Region "Eigenschaften"
 
     'Eigenschaften
@@ -161,12 +164,25 @@ Public MustInherit Class Dateiformat
         Get
             For i As Integer = 0 To Me.Zeitreihen.GetUpperBound(0)
                 If (Me.Zeitreihen(i).Title = title) Then
+                    'Zeitreihe zurückgeben
                     Return Me.Zeitreihen(i)
                 End If
             Next
-            'Zeitreihe nicht vorhanden
-            'TODO: Throw Exception?
-            Return New Zeitreihe("unbekannt")
+            'Zeitreihe ist noch nicht eingelesen!
+            'Vielleicht ist die Zeitreihe trotzdem in der Datei vorhanden?
+            For Each spalte As String In Me.YSpalten
+                If (spalte = title) Then
+                    'gewünschte Zeitreihe zur Spaltenauswahl hinzufügen
+                    ReDim Preserve Me.SpaltenSel(Me.SpaltenSel.GetUpperBound(0) + 1)
+                    Me.SpaltenSel(Me.SpaltenSel.GetUpperBound(0)) = title
+                    'Zeitreihen erneut einlesen
+                    Call Me.Read_File()
+                    'Zeitreihe zurückgeben
+                    Return Me.getReihe(title)
+                End If
+            Next
+            'Zeitreihe nicht vorhanden!
+            Throw New Exception("Zeitreihe '" & title & "' in Datei '" & System.IO.Path.GetFileName(Me.File) & "' nicht gefunden!")
         End Get
     End Property
 
@@ -180,6 +196,9 @@ Public MustInherit Class Dateiformat
     'Konstruktor
     '***********
     Public Sub New(ByVal FileName As String)
+
+        'Objektstruktur initialisieren
+        ReDim Me.Zeitreihen(-1)
 
         'Dateinamen setzen
         Me.File = FileName
