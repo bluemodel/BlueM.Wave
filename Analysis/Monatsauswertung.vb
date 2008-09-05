5a83c179bb7f93ec0e5b87f4fce7877d4ab3ff3b
froehlich <f.froehlich@bluemodel.org>
2008-09-05 08:20:49 +0000

    Code-Formatierung

--- Analysis/Monatsauswertung.vb
+++ Analysis/Monatsauswertung.vb
@@ -1,17 +1,18 @@
-'************************************************************************************
-'Analysiert eine Zeitreihe auf Monatsbasis:
-'Für jeden Monat werden Mittelwert, Median, Standardabweichung, Min und Max berechnet
-'************************************************************************************
+''' <summary>
+''' Berechnet monatliche Statistiken
+''' </summary>
+''' <remarks>http://130.83.196.154/BlueM/wiki/index.php/Wave:Monatsauswertung</remarks>
 Public Class Monatsauswertung
     Inherits Analysis
 
+#Region "Datenstrukturen"
+
     ''' <summary>
-    ''' Struktur für Monatswerte
+    ''' Struktur zum Speichern der Werte eines Monats
     ''' </summary>
-    ''' <remarks></remarks>
     Private Structure struct_Monatswerte
         Dim monatsname As String
-        Dim monatsort As Integer
+        Dim monatsindex As Integer
         Dim werte As List(Of Double)
         Dim mittelwert As Double
         Dim stdabw As Double
@@ -21,66 +22,24 @@ Public Class Monatsauswertung
     End Structure
 
     ''' <summary>
-    ''' Ergebnisstruktur
+    ''' Struktur zum Speichern der Werte mehrerer Monate
     ''' </summary>
-    ''' <remarks></remarks>
     Private Structure struct_Ergebnis
         Dim Monatswerte() As struct_Monatswerte
     End Structure
+
+#End Region 'Datenstrukturen
+
+#Region "Eigenschaften"
 
     ''' <summary>
     ''' internes Ergebnis
     ''' </summary>
     Private mErgebnis As struct_Ergebnis
 
-    ''' <summary>
-    ''' Konstruktor
-    ''' </summary>
-    ''' <param name="zeitreihen"></param>
-    ''' <remarks></remarks>
-    Public Sub New(ByRef zeitreihen As Collection)
+#End Region 'Eigenschaften
 
-        Call MyBase.New(zeitreihen)
-
-        'Prüfung: Genau 1 Zeitreihe erlaubt
-        If (zeitreihen.Count <> 1) Then
-            Throw New Exception("Bei der Monatsauswertung muss genau 1 Zeitreihe ausgewählt werden!")
-        End If
-
-        Dim i As Integer
-
-        'Ergebnisstruktur instanzieren
-        ReDim Me.mErgebnis.Monatswerte(11)
-        Me.mErgebnis.Monatswerte(0).monatsname = "Januar"
-        Me.mErgebnis.Monatswerte(0).monatsort = 3
-        Me.mErgebnis.Monatswerte(1).monatsname = "Februar"
-        Me.mErgebnis.Monatswerte(1).monatsort = 4
-        Me.mErgebnis.Monatswerte(2).monatsname = "März"
-        Me.mErgebnis.Monatswerte(2).monatsort = 5
-        Me.mErgebnis.Monatswerte(3).monatsname = "April"
-        Me.mErgebnis.Monatswerte(3).monatsort = 6
-        Me.mErgebnis.Monatswerte(4).monatsname = "Mai"
-        Me.mErgebnis.Monatswerte(4).monatsort = 7
-        Me.mErgebnis.Monatswerte(5).monatsname = "Juni"
-        Me.mErgebnis.Monatswerte(5).monatsort = 8
-        Me.mErgebnis.Monatswerte(6).monatsname = "Juli"
-        Me.mErgebnis.Monatswerte(6).monatsort = 9
-        Me.mErgebnis.Monatswerte(7).monatsname = "August"
-        Me.mErgebnis.Monatswerte(7).monatsort = 10
-        Me.mErgebnis.Monatswerte(8).monatsname = "September"
-        Me.mErgebnis.Monatswerte(8).monatsort = 11
-        Me.mErgebnis.Monatswerte(9).monatsname = "Oktober"
-        Me.mErgebnis.Monatswerte(9).monatsort = 12
-        Me.mErgebnis.Monatswerte(10).monatsname = "November"
-        Me.mErgebnis.Monatswerte(10).monatsort = 1
-        Me.mErgebnis.Monatswerte(11).monatsname = "Dezember"
-        Me.mErgebnis.Monatswerte(11).monatsort = 2
-
-        For i = 0 To 11
-            Me.mErgebnis.Monatswerte(i).werte = New List(Of Double)
-        Next
-
-    End Sub
+#Region "Properties"
 
     ''' <summary>
     ''' Flag, der anzeigt, ob die Analysefunktion einen Ergebnistext erzeugt
@@ -108,6 +67,58 @@ Public Class Monatsauswertung
             Return True
         End Get
     End Property
+
+#End Region ' Properties
+
+#Region "Methoden"
+
+    ''' <summary>
+    ''' Konstruktor
+    ''' </summary>
+    ''' <param name="zeitreihen">zu analysierende Zeitreihen</param>
+    Public Sub New(ByRef zeitreihen As Collection)
+
+        Call MyBase.New(zeitreihen)
+
+        'Prüfung: Genau 1 Zeitreihe erlaubt
+        If (zeitreihen.Count <> 1) Then
+            Throw New Exception("Bei der Monatsauswertung muss genau 1 Zeitreihe ausgewählt werden!")
+        End If
+
+        Dim i As Integer
+
+        'Ergebnisstruktur instanzieren
+        ReDim Me.mErgebnis.Monatswerte(11)
+        Me.mErgebnis.Monatswerte(0).monatsname = "Januar"
+        Me.mErgebnis.Monatswerte(0).monatsindex = 3
+        Me.mErgebnis.Monatswerte(1).monatsname = "Februar"
+        Me.mErgebnis.Monatswerte(1).monatsindex = 4
+        Me.mErgebnis.Monatswerte(2).monatsname = "März"
+        Me.mErgebnis.Monatswerte(2).monatsindex = 5
+        Me.mErgebnis.Monatswerte(3).monatsname = "April"
+        Me.mErgebnis.Monatswerte(3).monatsindex = 6
+        Me.mErgebnis.Monatswerte(4).monatsname = "Mai"
+        Me.mErgebnis.Monatswerte(4).monatsindex = 7
+        Me.mErgebnis.Monatswerte(5).monatsname = "Juni"
+        Me.mErgebnis.Monatswerte(5).monatsindex = 8
+        Me.mErgebnis.Monatswerte(6).monatsname = "Juli"
+        Me.mErgebnis.Monatswerte(6).monatsindex = 9
+        Me.mErgebnis.Monatswerte(7).monatsname = "August"
+        Me.mErgebnis.Monatswerte(7).monatsindex = 10
+        Me.mErgebnis.Monatswerte(8).monatsname = "September"
+        Me.mErgebnis.Monatswerte(8).monatsindex = 11
+        Me.mErgebnis.Monatswerte(9).monatsname = "Oktober"
+        Me.mErgebnis.Monatswerte(9).monatsindex = 12
+        Me.mErgebnis.Monatswerte(10).monatsname = "November"
+        Me.mErgebnis.Monatswerte(10).monatsindex = 1
+        Me.mErgebnis.Monatswerte(11).monatsname = "Dezember"
+        Me.mErgebnis.Monatswerte(11).monatsindex = 2
+
+        For i = 0 To 11
+            Me.mErgebnis.Monatswerte(i).werte = New List(Of Double)
+        Next
+
+    End Sub
 
     'Analyse ausführen
     '*****************
@@ -156,7 +167,7 @@ Public Class Monatsauswertung
                         .median = .werte(((N + 1) / 2) - 1)
                     End If
                 Else
-                    MsgBox("Für den Monat " & .monatsname & " liegen keine Werte vor!", MsgBoxStyle.Exclamation, "Warnung")
+                    MsgBox("Für den Monat " & .monatsname & " liegen keine Werte vor!", MsgBoxStyle.Exclamation)
                 End If
 
             End With
@@ -199,7 +210,7 @@ Public Class Monatsauswertung
         minmax.HighBrush.Color = Color.LightGray
         minmax.HighBrush.Transparency = 50
         For i = 0 To 11
-            minmax.Add(Me.mErgebnis.Monatswerte(i).monatsort, Me.mErgebnis.Monatswerte(i).max, Me.mErgebnis.Monatswerte(i).min, Me.mErgebnis.Monatswerte(i).monatsname)
+            minmax.Add(Me.mErgebnis.Monatswerte(i).monatsindex, Me.mErgebnis.Monatswerte(i).max, Me.mErgebnis.Monatswerte(i).min, Me.mErgebnis.Monatswerte(i).monatsname)
         Next
 
         'Standardabweichung
@@ -208,7 +219,7 @@ Public Class Monatsauswertung
         stdabw.Color = Color.Red
         stdabw.ErrorWidth = 50
         For i = 0 To 11
-            stdabw.Add(Me.mErgebnis.Monatswerte(i).monatsort, Me.mErgebnis.Monatswerte(i).mittelwert, Me.mErgebnis.Monatswerte(i).stdabw, Me.mErgebnis.Monatswerte(i).monatsname)
+            stdabw.Add(Me.mErgebnis.Monatswerte(i).monatsindex, Me.mErgebnis.Monatswerte(i).mittelwert, Me.mErgebnis.Monatswerte(i).stdabw, Me.mErgebnis.Monatswerte(i).monatsname)
         Next
 
         'Mittelwert
@@ -217,7 +228,7 @@ Public Class Monatsauswertung
         mittelwert.Color = Color.Blue
         mittelwert.LinePen.Width = 2
         For i = 0 To 11
-            mittelwert.Add(Me.mErgebnis.Monatswerte(i).monatsort, Me.mErgebnis.Monatswerte(i).mittelwert, Me.mErgebnis.Monatswerte(i).monatsname)
+            mittelwert.Add(Me.mErgebnis.Monatswerte(i).monatsindex, Me.mErgebnis.Monatswerte(i).mittelwert, Me.mErgebnis.Monatswerte(i).monatsname)
         Next
 
         'Median
@@ -225,9 +236,11 @@ Public Class Monatsauswertung
         median.Title = "Median"
         median.Color = Color.Green
         For i = 0 To 11
-            median.Add(Me.mErgebnis.Monatswerte(i).monatsort, Me.mErgebnis.Monatswerte(i).median, Me.mErgebnis.Monatswerte(i).monatsname)
+            median.Add(Me.mErgebnis.Monatswerte(i).monatsindex, Me.mErgebnis.Monatswerte(i).median, Me.mErgebnis.Monatswerte(i).monatsname)
         Next
 
     End Sub
+
+#End Region 'Methoden
 
 End Class
