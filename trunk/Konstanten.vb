8c8d32f0c8a9d0fffb7572bf85c4c554b0b414e4
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2008-09-05 15:31:26 +0000

    * MessageDialog ist jetzt LogWindow * Refactoring

--- trunk/Konstanten.vb
+++ trunk/Konstanten.vb
@@ -2,61 +2,66 @@ Imports System.Globalization
 
 Module Konstanten
 
+    'Log-Fenster
+    Friend Log As LogWindow
+
     'Zeilenumbruch
     Public Const eol As String = Chr(13) & Chr(10)
 
     'Not a Number
     Public Const NaN As Double = -999
 
+    'Datumsformat
+    Public Const Datumsformat As String = "dd.MM.yyyy HH:mm"
+
+    'Zahlenformatanweisung
+    Public ReadOnly Property Zahlenformat() As NumberFormatInfo
+        Get
+            'NumberFormatInfo einrichten
+            Zahlenformat = New NumberFormatInfo()
+            Zahlenformat.NumberDecimalSeparator = "."
+            Zahlenformat.NumberGroupSeparator = ""
+            Zahlenformat.NumberGroupSizes = New Integer() {3}
+        End Get
+    End Property
+
     'Zeitreihenformate
-    Public Enum Formate As Integer
+    Public Enum Dateiformate As Integer
         ZRE = 1
         WEL = 2
         CSV = 3
         ASC = 4
     End Enum
 
-    Public Const Datumsformat As String = "dd.MM.yyyy HH:mm"
-
-    'Message-Dialog
-    Friend MsgDialog As MessageDialog
-
-    'Zahlenformatanweisung
-    Public ReadOnly Property FortranProvider() As NumberFormatInfo
-        Get
-            'Provider einrichten
-            FortranProvider = New NumberFormatInfo()
-            FortranProvider.NumberDecimalSeparator = "."
-            FortranProvider.NumberGroupSeparator = ""
-            FortranProvider.NumberGroupSizes = New Integer() {3}
-        End Get
-    End Property
-
-    'Umwandlung von String zu Double
-    '*******************************
+    ''' <summary>
+    ''' Wandelt einen String zu Double um
+    ''' </summary>
+    ''' <param name="str">umzuwandelnder String</param>
+    ''' <returns>Double-Wert, NaN falls String nicht umwandelbar</returns>
+    ''' <remarks></remarks>
     Public Function StringToDouble(ByVal str As String) As Double
 
         Dim wert As Double
 
         Try
-            wert = Convert.ToDouble(str, Konstanten.FortranProvider)
+            wert = Convert.ToDouble(str, Konstanten.Zahlenformat)
         Catch ex As Exception
             wert = Konstanten.NaN
-            Call AddMessage("Der Wert '" & str & "' wurde durch " & Konstanten.NaN.ToString() & " ersetzt!")
+            Call AddLogEntry("Der Wert '" & str & "' konnte nicht gelesen werden und wurde durch NaN (" & Konstanten.NaN.ToString() & ") ersetzt!")
         End Try
 
         Return wert
 
     End Function
 
-    'Message hinzufügen
-    '******************
-    Public Sub AddMessage(ByVal msg As String)
+    ''' <summary>
+    ''' Einen Eintrag zum Log hinzufügen
+    ''' </summary>
+    ''' <param name="msg">Der hinzuzufügende Log-Eintrag</param>
+    Public Sub AddLogEntry(ByVal msg As String)
 
-        Wave.ToolStripStatusLabel_Messages.Enabled = True
-
-        'Message hinzufügen
-        Call MsgDialog.AddMessage(msg)
+        'Eintrag hinzufügen
+        Call Log.AddLogEntry(msg)
 
     End Sub
 
