65c0c034437e5e8f76261d64479252b060e56d4c
bach <bach@6b41527d-9311-48e1-90e2-fe324f485066>
2008-12-08 23:59:27 +0000

    Gegenueberstellung: - Es werden nur noch gemeinsame Stützstellen berücksichtigt - Einheit der Zeitreihen wird an Achse geschrieben

--- trunk/Analysis/Gegenueberstellung.vb
+++ trunk/Analysis/Gegenueberstellung.vb
@@ -63,20 +63,18 @@ Public Class Gegenueberstellung
         reihe2 = Me.mZeitreihen(2)
 
         'nur gemeinsame Stützstellen nutzen
-        'values = AnalysisHelper.getConcurrentValues(reihe1, reihe2)
+        values = AnalysisHelper.getConcurrentValues(reihe1, reihe2)
+        If (values.GetUpperBound(0) + 1 < reihe1.Length) Then  ' + Weil value-Array von 0 zählt
+            MessageBox.Show("ACHTUNG: Es wurden Werte entfernt!")
+        End If
 
-        ReDim Me.ergebnisreihe(reihe1.Length, 1)
-        'ReDim Me.summe1(values.GetUpperBound(0))
-        'ReDim Me.summe2(values.GetUpperBound(0))
+        ' Ergebnisreihe allokieren
+        ReDim Me.ergebnisreihe(values.GetUpperBound(0), 1)
 
-        'Erster Wert
-        'Me.summe1(0) = values(0, 0)
-        'Me.summe2(0) = values(0, 1)
-
-        'Weitere Werte kumulativ aufsummieren
-        For i = 0 To reihe1.Length - 1
-            ergebnisreihe(i, 0) = reihe1.YWerte(i)
-            ergebnisreihe(i, 1) = reihe2.YWerte(i)
+        ' x- und y-Werte der Ergebnisreihe zuweisen
+        For i = 0 To values.GetUpperBound(0)
+            ergebnisreihe(i, 0) = values(i, 0)
+            ergebnisreihe(i, 1) = values(i, 1)
         Next
 
         'Datume übernehmen (werden später für Punkte-Labels im Diagramm gebraucht)
@@ -99,7 +97,7 @@ Public Class Gegenueberstellung
         'Diagramm:
         '---------
         Dim i, ende As Integer
-        Dim gegenueberstellung_linie, gerade As Steema.TeeChart.Styles.Line
+        Dim gegenueberstellung_linie As Steema.TeeChart.Styles.Line
 
         Me.mResultChart = New Steema.TeeChart.Chart()
         Me.mResultChart.Aspect.View3D = False
@@ -109,9 +107,9 @@ Public Class Gegenueberstellung
 
         'Achsen
         '------
-        Me.mResultChart.Axes.Bottom.Title.Caption = Me.mZeitreihen(1).Title
+        Me.mResultChart.Axes.Bottom.Title.Caption = Me.mZeitreihen(1).Title & " [" & Me.mZeitreihen(1).Einheit & "]"
         Me.mResultChart.Axes.Bottom.Labels.Style = Steema.TeeChart.AxisLabelStyle.Value
-        Me.mResultChart.Axes.Left.Title.Caption = Me.mZeitreihen(2).Title
+        Me.mResultChart.Axes.Left.Title.Caption = Me.mZeitreihen(2).Title & " [" & Me.mZeitreihen(2).Einheit & "]"
         Me.mResultChart.Axes.Left.Labels.Style = Steema.TeeChart.AxisLabelStyle.Value
 
         'Reihen
@@ -123,20 +121,11 @@ Public Class Gegenueberstellung
         gegenueberstellung_linie.Pointer.HorizSize = 2
         gegenueberstellung_linie.Pointer.VertSize = 2
 
-        'gerade = New Steema.TeeChart.Styles.Line(Me.mResultChart)
-        'gerade.Title = "45° Gerade"
-        'gerade.Color = Color.DarkGray
-        'gerade.LinePen.Style = Drawing2D.DashStyle.Dash
-
         'Werte eintragen
         ende = (ergebnisreihe.Length / 2 - 2)
         For i = 0 To ende
             gegenueberstellung_linie.Add(ergebnisreihe(i, 0), ergebnisreihe(i, 1), datume(i).ToString(Konstanten.Datumsformat))
         Next
-
-        'gerade.Add(0, 0)
-        'Dim maxwert As Double = Math.Min(Me.mResultChart.Axes.Bottom.MaxXValue, Me.mResultChart.Axes.Left.MaxYValue)
-        'gerade.Add(maxwert, maxwert)
 
         'Markstips
         '---------
