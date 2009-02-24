e744435be54a8bfc8fcaf8f4e9f936e1eaff7c9a
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2009-02-24 14:13:40 +0000

    GoodnessOfFit: Dialog hinzugefügt, mit dem man die "gemessene" Zeitreihe bestimmen kann

---
+++ trunk/Analysis/GoodnessOfFit_Dialog.vb
@@ -0,0 +1,25 @@
+﻿Public Class GoodnessOfFit_Dialog
+
+    Public Sub New(ByVal zre1 As String, ByVal zre2 As String)
+
+        ' This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.RadioButton1.Text = zre1
+        Me.RadioButton2.Text = zre2
+
+    End Sub
+
+    Public Function getNrGemesseneReihe() As Integer
+
+        If (Me.RadioButton1.Checked) Then
+            Return 1
+        Else
+            Return 2
+        End If
+
+    End Function
+
+
+End Class
