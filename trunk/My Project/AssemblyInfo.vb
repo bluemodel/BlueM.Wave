121e57310c06081878d16d68d9dcbf0425a29aa5
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2009-05-27 09:14:40 +0000

    v1.3: AssemblyInfo

--- trunk/My Project/AssemblyInfo.vb
+++ trunk/My Project/AssemblyInfo.vb
@@ -36,4 +36,4 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.*")> 
+<Assembly: AssemblyVersion("1.3.*")> 
