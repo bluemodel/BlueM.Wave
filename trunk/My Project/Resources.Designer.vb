8387bdf3be2503322ccb07c70635496f2a732774
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2011-09-14 22:28:55 +0000

    BlueM Icon hinzugefügt und anderes altes Bild entfernt

--- trunk/My Project/Resources.Designer.vb
+++ trunk/My Project/Resources.Designer.vb
@@ -81,10 +81,17 @@ Namespace My.Resources
             End Get
         End Property
         
+        Friend ReadOnly Property BlueM_icon() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("BlueM_icon", resourceCulture)
+                Return CType(obj, System.Drawing.Bitmap)
+            End Get
+        End Property
+
         Friend ReadOnly Property calculator() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("calculator", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
+                Return CType(obj, System.Drawing.Bitmap)
             End Get
         End Property
         
