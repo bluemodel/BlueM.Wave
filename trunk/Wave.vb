4852adce5c06a1d3a39212aafcd5c6aef87a1e5e
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2013-10-30 23:22:03 +0000

    Legende im Übersichtsdiagramm standardmäßig ausschalten

--- trunk/Wave.vb
+++ trunk/Wave.vb
@@ -167,6 +167,7 @@ Public Class Wave
         Me.TChart2.Panel.Brush.Color = Color.FromArgb(239,239,239)
         Me.TChart2.Walls.Back.Color = Color.FromArgb(239, 239, 239)
         Me.TChart2.Header.Visible = False
+        Me.TChart2.Legend.Visible = False
 
         'Übersicht darf nicht gescrolled oder gezoomt werden
         Me.TChart2.Zoom.Allow = False
