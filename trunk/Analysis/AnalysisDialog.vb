02101d1c2a65b723465dec029e8608c2345f2438
bach <bach@6b41527d-9311-48e1-90e2-fe324f485066>
2014-05-20 11:54:05 +0000

    Analysis Dialog size increased for easier choose of time series if there are many time series within the diagram

--- trunk/Analysis/AnalysisDialog.vb
+++ trunk/Analysis/AnalysisDialog.vb
@@ -76,4 +76,7 @@ Public Class AnalysisDialog
     End Sub
 
 
+    Private Sub AnalysisDialog_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
