7da980a4f6afaaf9af81b3244f8569ea7198d895
froehlich <f.froehlich@bluemodel.org>
2016-11-07 09:49:18 +0000

    Remove everything to do with the Leitfaden-version of Wave. Bug 690

--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -53,27 +53,6 @@ Namespace My
                 Return defaultInstance
             End Get
         End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=DIRC;Integrated Security=True;Pooling=False"& _ 
-            "")>  _
-        Public ReadOnly Property DIRCConnectionString() As String
-            Get
-                Return CType(Me("DIRCConnectionString"),String)
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("Data Source=IHWB59;Initial Catalog=BlueMOrb;Integrated Security=True")>  _
-        Public ReadOnly Property BlueMOrbConnectionString() As String
-            Get
-                Return CType(Me("BlueMOrbConnectionString"),String)
-            End Get
-        End Property
     End Class
 End Namespace
 
