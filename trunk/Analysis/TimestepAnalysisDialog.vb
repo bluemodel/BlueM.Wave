da4917cbe69736c9dfedcc76dc99a5c30b644cea
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2019-12-13 13:29:56 +0000

    Added a new analysis function "TimestepAnalysis" which computes the time difference between each node of one or multiple time series and displays them as new time series

---
+++ trunk/Analysis/TimestepAnalysisDialog.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Windows.Forms
+
+Partial Friend Class TimestepAnalysisDialog
+
+    Private zres As Dictionary(Of String, TimeSeries)
+    Private isInitializing As Boolean
+
+    Public Sub New()
+        Call InitializeComponent()
+
+        'fill comboboxes
+        Me.isInitializing = True
+        'Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Years)
+        'Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Months)
+        Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Days)
+        Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Hours)
+        Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Minutes)
+        Me.ComboBox_TimestepType.Items.Add(TimeStepAnalysis.TimeUnit.Seconds)
+        Me.ComboBox_TimestepType.SelectedItem = TimeStepAnalysis.TimeUnit.Minutes
+        Me.isInitializing = False
+
+    End Sub
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+End Class
--- trunk/My Project/AssemblyInfo.vb
+++ trunk/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.7.0.*")> 
+<Assembly: AssemblyVersion("1.7.1.*")> 
