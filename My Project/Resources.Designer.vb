059a0db39e6bfc9b08d557454eb3f5ff8da84aaa
froehlich <f.froehlich@bluemodel.org>
2021-01-30 21:45:17 +0000

    Added an update notification and a check for update help menu item. Check for update is performed at startup and when menu item is clicked.

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property application_get() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("application_get", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property application_split() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("application_split", resourceCulture)
