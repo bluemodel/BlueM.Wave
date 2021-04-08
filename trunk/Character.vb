5422119a0538fb98ecc76369a8b2107cd7c63205
froehlich <froehlich@6b41527d-9311-48e1-90e2-fe324f485066>
2021-04-08 15:59:16 +0000

    Wave project files (WVP): allow specification of words such as "tab" as separator and the use of quotes for series names containing ":"

--- trunk/Character.vb
+++ trunk/Character.vb
@@ -36,10 +36,25 @@ Public Class Character
     ''' <summary>
     ''' Constructor
     ''' </summary>
-    ''' <param name="str">String to convert (only the first character is used)</param>
+    ''' <param name="str">String to convert (only the first character is used)
+    ''' Special strings such as " ", "space", "tab", "point", "comma" and "semicolon" are also possible
+    ''' </param>
     ''' <remarks></remarks>
     Public Sub New(ByVal str As String)
-        ToChar = Convert.ToChar(str)
+        Select Case str.Trim().ToLower()
+            Case "", "space"
+                _char = Chr(32)
+            Case "tab"
+                _char = Chr(9)
+            Case "point"
+                _char = Chr(46)
+            Case "comma"
+                _char = Chr(44)
+            Case "semicolon"
+                _char = Chr(59)
+            Case Else
+                _char = Convert.ToChar(str)
+        End Select
     End Sub
 
     ''' <summary>
