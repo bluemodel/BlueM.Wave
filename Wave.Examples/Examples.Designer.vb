495b5cc57b0311b05281a1775985c58c3fd15a91
jamaa <90166+jamaa@users.noreply.github.com>
2022-10-09 13:25:55 +0200

    added project Wave.Examples with examples of API usage

---
+++ Wave.Examples/Examples.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Examples
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox_File = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 51)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Example1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 80)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Example2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox_File
+        '
+        Me.TextBox_File.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox_File.Location = New System.Drawing.Point(130, 13)
+        Me.TextBox_File.Name = "TextBox_File"
+        Me.TextBox_File.Size = New System.Drawing.Size(542, 20)
+        Me.TextBox_File.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 110)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Example3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(13, 140)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Example4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Path to time series file:"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 169)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Example5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Examples
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 245)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox_File)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Examples"
+        Me.Text = "Wave.Examples"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox_File As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button5 As Button
+End Class
---
+++ Wave.Examples/Examples.vb
@@ -0,0 +1,170 @@
+﻿Public Class Examples
+
+    ''' <summary>
+    ''' Path to the time series file
+    ''' </summary>
+    ''' <returns></returns>
+    Private ReadOnly Property FilePath As String
+        Get
+            Return Me.TextBox_File.Text
+        End Get
+    End Property
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        'Attempt to determine the path to an example time series file from BlueM.Datasets
+        Try
+            Dim appdir As String = My.Application.Info.DirectoryPath() 'e.g. BlueM.Wave\Wave.Examples\bin\x64\Debug
+            Dim testdatadir As String = appdir
+            'move up 5 levels
+            For i As Integer = 1 To 5
+                testdatadir = IO.Directory.GetParent(testdatadir).FullName
+            Next
+            testdatadir = IO.Path.Combine(testdatadir, "BlueM.Datasets", "Wave")
+            Dim filePath As String = IO.Path.Combine(testdatadir, "WEL", "DEMONA_PSI.wel")
+            If Not IO.File.Exists(filePath) Then
+                Throw New Exception($"File not found: {filePath}")
+            End If
+            Me.TextBox_File.Text = filePath
+        Catch ex As Exception
+            MsgBox("Unable to locate test file! " &
+                   "Expected directory: BlueM.Datasets\Wave in the same directory as BlueM.Wave! " &
+                   ex.Message,
+                   MsgBoxStyle.Exclamation
+            )
+        End Try
+
+        'Add button click handlers
+        AddHandler Button1.Click, AddressOf Example1
+        AddHandler Button2.Click, AddressOf Example2
+        AddHandler Button3.Click, AddressOf Example3
+        AddHandler Button4.Click, AddressOf Example4
+        AddHandler Button5.Click, AddressOf Example5
+
+    End Sub
+
+    Private Sub Example1()
+
+        Dim Wave1 As New BlueM.Wave.Wave()
+
+        'import a time series file and then display Wave
+        Wave1.Import_File(Me.FilePath) 'depending on the file format, this may display an import dialog
+        Wave1.Show()
+
+    End Sub
+
+    Private Sub Example2()
+
+        Dim Wave1 As New BlueM.Wave.Wave()
+        Dim myFile As BlueM.Wave.TimeSeriesFile
+        Dim ts1, ts2, ts3 As BlueM.Wave.TimeSeries
+
+        'get a file instance
+        myFile = BlueM.Wave.TimeSeriesFile.getInstance(Me.FilePath)
+
+        'get a time series by name (this will automatically attempt to read the series from the file if not already done)
+        ts1 = myFile.getTimeSeries("AA  _ETA")
+
+        'get a second time series
+        ts2 = myFile.getTimeSeries("AA  _ETP")
+
+        'get a third time series by column index
+        ts3 = myFile.getTimeSeries(83)
+
+        'import the time series in Wave
+        Wave1.Import_Series(ts1)
+        Wave1.Import_Series(ts2)
+        Wave1.Import_Series(ts3)
+
+        Wave1.Show()
+
+    End Sub
+
+    Private Sub Example3()
+
+        Dim Wave1 As New BlueM.Wave.Wave()
+        Dim myFile As BlueM.Wave.TimeSeriesFile
+        Dim ts As BlueM.Wave.TimeSeries
+
+        'get a file instance
+        myFile = BlueM.Wave.TimeSeriesFile.getInstance(Me.FilePath)
+
+        'check the time series contained in the file
+        For Each sInfo As BlueM.Wave.TimeSeriesInfo In myFile.TimeSeriesInfos
+            Console.WriteLine(sInfo.Name)
+        Next
+
+        'select all series for import
+        myFile.selectAllSeries()
+
+        'read the selected time series from the file
+        myFile.readFile()
+
+        'loop over all series read from the file and print some information about them
+        For Each ts In myFile.TimeSeries.Values
+            Console.WriteLine("Series title: " & ts.Title)
+            Console.WriteLine("Extent: " & ts.StartDate & " - " & ts.EndDate)
+            Console.WriteLine("Max value: " & ts.Maximum)
+            Console.WriteLine("Min value: " & ts.Minimum)
+        Next
+
+        'get one particular time series
+        ts = myFile.getTimeSeries("S1  _1AB")
+
+        'Display the series in Wave
+        Wave1.Import_Series(ts)
+        Wave1.Show()
+
+    End Sub
+
+    Private Sub Example4()
+
+        Dim Wave1 As New BlueM.Wave.Wave()
+
+        'create a new time series
+        Dim ts As New BlueM.Wave.TimeSeries("my series")
+        ts.Unit = "m3/s"
+        ts.Interpretation = BlueM.Wave.TimeSeries.InterpretationEnum.BlockRight
+
+        'add some nodes to the time series
+        ts.AddNode(New DateTime(2000, 1, 1), 10)
+        ts.AddNode(New DateTime(2000, 1, 2), 20)
+        ts.AddNode(New DateTime(2000, 1, 3), 30)
+        ts.AddNode(New DateTime(2000, 1, 4), 15)
+
+        'display the series in Wave
+        Wave1.Import_Series(ts)
+        Wave1.Show()
+
+    End Sub
+
+    Private Sub Example5()
+
+        Dim myFile As BlueM.Wave.TimeSeriesFile
+        Dim ts As BlueM.Wave.TimeSeries
+
+        'get a file instance
+        myFile = BlueM.Wave.TimeSeriesFile.getInstance(Me.FilePath)
+
+        'get a specific time series from the file
+        ts = myFile.getTimeSeries("S1  _1AB")
+
+        'cut the time series
+        ts.Cut(New DateTime(1959, 1, 1), New DateTime(1959, 2, 1))
+
+        'loop over the nodes of the time series and print them
+        Dim d As DateTime
+        Dim v As Double
+        For Each node As KeyValuePair(Of DateTime, Double) In ts.Nodes
+            d = node.Key
+            v = node.Value
+            Console.WriteLine($"{d}: {v}")
+        Next
+
+    End Sub
+
+End Class
