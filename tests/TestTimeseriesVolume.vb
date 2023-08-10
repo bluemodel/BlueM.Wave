d5208411bbce271563e7f9ef3e1f44f5b5624176
jamaa <90166+jamaa@users.noreply.github.com>
2023-08-10 22:45:34 +0200

    add more tests and name them properly

--- tests/TestTimeseriesVolume.vb
+++ tests/TestTimeseriesVolume.vb
@@ -26,7 +26,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     ''' <summary>
     ''' Volume for time series with unit m³/s, interpretation Instantaneous
     ''' </summary>
-    <TestMethod()> Public Sub TestVolume_1()
+    <TestMethod()> Public Sub TestVolume_m3s_instantaneous()
 
         Dim ts As New TimeSeries()
         ts.Unit = "m³/s"
@@ -43,7 +43,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     ''' <summary>
     ''' Volume for time series with unit m³/s, interpretation BlockRight
     ''' </summary>
-    <TestMethod()> Public Sub TestVolume_2()
+    <TestMethod()> Public Sub TestVolume_m3s_blockright()
 
         Dim ts As New TimeSeries()
         ts.Unit = "m³/s"
@@ -60,7 +60,7 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     ''' <summary>
     ''' Volume for time series with unit m³/s, interpretation BlockLeft
     ''' </summary>
-    <TestMethod()> Public Sub TestVolume_3()
+    <TestMethod()> Public Sub TestVolume_m3s_blockleft()
 
         Dim ts As New TimeSeries()
         ts.Unit = "m³/s"
@@ -75,26 +75,9 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
     End Sub
 
     ''' <summary>
-    ''' Volume for time series with unit mm, interpretation CumulativePerTimestep
-    ''' </summary>
-    <TestMethod()> Public Sub TestVolume_4()
-
-        Dim ts As New TimeSeries()
-        ts.Unit = "mm"
-        ts.Interpretation = TimeSeries.InterpretationEnum.CumulativePerTimestep
-        ts.AddNode(New DateTime(2000, 1, 1), 1.0)
-        ts.AddNode(New DateTime(2000, 1, 2), 2.0)
-        ts.AddNode(New DateTime(2000, 1, 3), 3.0)
-        ts.AddNode(New DateTime(2000, 1, 4), 2.0)
-
-        Assert.AreEqual(8.0, ts.Volume)
-
-    End Sub
-
-    ''' <summary>
     ''' Volume for time series with unit mm/h, interpretation BlockRight
     ''' </summary>
-    <TestMethod()> Public Sub TestVolume_5()
+    <TestMethod()> Public Sub TestVolume_mmh_blockright()
 
         Dim ts As New TimeSeries()
         ts.Unit = "mm/h"
@@ -105,6 +88,77 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         ts.AddNode(New DateTime(2000, 1, 4), 2.0)
 
         Assert.AreEqual(144.0, ts.Volume)
+
+    End Sub
+
+    ''' <summary>
+    ''' Volume for time series with unit mm/d, interpretation BlockRight
+    ''' </summary>
+    <TestMethod()> Public Sub TestVolume_mmd_blockright()
+
+        Dim ts As New TimeSeries()
+        ts.Unit = "mm/d"
+        ts.Interpretation = TimeSeries.InterpretationEnum.BlockRight
+        ts.AddNode(New DateTime(2000, 1, 1), 1.0)
+        ts.AddNode(New DateTime(2000, 1, 2), 2.0)
+        ts.AddNode(New DateTime(2000, 1, 3), 3.0)
+        ts.AddNode(New DateTime(2000, 1, 4), 2.0)
+
+        Assert.AreEqual(6.0, ts.Volume)
+
+    End Sub
+
+    ''' <summary>
+    ''' Volume for time series with unsupported units
+    ''' </summary>
+    <TestMethod()> Public Sub TestVolume_unsupported_units()
+
+        Dim units As New List(Of String) From
+            {
+            "mm/m",
+            "m",
+            "mm",
+            "-",
+            "mm/y",
+            "mm/month"
+            }
+
+        Dim ts As New TimeSeries()
+        ts.Interpretation = TimeSeries.InterpretationEnum.BlockRight
+        ts.AddNode(New DateTime(2000, 1, 1), 1.0)
+        ts.AddNode(New DateTime(2000, 1, 2), 2.0)
+        ts.AddNode(New DateTime(2000, 1, 3), 3.0)
+        ts.AddNode(New DateTime(2000, 1, 4), 2.0)
+
+        For Each unit As String In units
+            ts.Unit = unit
+            Assert.AreEqual(Double.NaN, ts.Volume)
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Volume for time series with unsupported interpretations
+    ''' </summary>
+    <TestMethod()> Public Sub TestVolume_unsupported_interpretations()
+
+        Dim interpretations As New List(Of TimeSeries.InterpretationEnum) From
+            {
+                TimeSeries.InterpretationEnum.Undefined,
+                TimeSeries.InterpretationEnum.Cumulative,
+                TimeSeries.InterpretationEnum.CumulativePerTimestep
+            }
+
+        Dim ts As New TimeSeries()
+        ts.Unit = "mm/h"
+        ts.AddNode(New DateTime(2000, 1, 1), 1.0)
+        ts.AddNode(New DateTime(2000, 1, 2), 2.0)
+        ts.AddNode(New DateTime(2000, 1, 3), 3.0)
+        ts.AddNode(New DateTime(2000, 1, 4), 2.0)
+
+        For Each interpretation As TimeSeries.InterpretationEnum In interpretations
+            ts.Interpretation = interpretation
+            Assert.AreEqual(Double.NaN, ts.Volume)
+        Next
 
     End Sub
 
