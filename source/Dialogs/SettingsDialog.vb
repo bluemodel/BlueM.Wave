e6a83cb03219a35c6abc016335d5affbf20ef824
jamaa <90166+jamaa@users.noreply.github.com>
2025-06-07 13:59:25 +0200

    change loggingLevel to a user setting and make it configurable in the settings dialog

--- source/Dialogs/SettingsDialog.vb
+++ source/Dialogs/SettingsDialog.vb
@@ -4,17 +4,22 @@ Public Class SettingsDialog
 
     Private Sub SettingsDialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'set values in the form
+        'initialize form
+        Me.ComboBox_loggingLevel.Items.AddRange([Enum].GetNames(GetType(Log.levels)))
+
+        'set current setting values in the form
         Me.NumericUpDown_DefaultLineWidth.Value = My.Settings.defaultLineWidth
+        Me.ComboBox_loggingLevel.SelectedItem = My.Settings.loggingLevel.ToString()
 
     End Sub
-
 
     Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
 
         'store the settings
         My.Settings.defaultLineWidth = CInt(Me.NumericUpDown_DefaultLineWidth.Value)
+        My.Settings.loggingLevel = Me.ComboBox_loggingLevel.SelectedItem.ToString()
 
+        'save them
         My.Settings.Save()
 
         Me.DialogResult = System.Windows.Forms.DialogResult.OK
