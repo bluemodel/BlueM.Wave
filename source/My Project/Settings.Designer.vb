1e48d25489d582c12d6002573ce55b86b7fa896a
jamaa <90166+jamaa@users.noreply.github.com>
2025-06-07 13:44:33 +0200

    add isNewVersion user property for upgrading user settings from previous versions, see https://dondraper.com/2011/01/easily-save-and-retrieve-application-and-user-settings-in-vb-net-or-c-apps/#comment-115

--- source/My Project/Settings.Designer.vb
+++ source/My Project/Settings.Designer.vb
@@ -74,6 +74,18 @@ Namespace My
                 Me("defaultLineWidth") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("True")>  _
+        Public Property isNewVersion() As Boolean
+            Get
+                Return CType(Me("isNewVersion"),Boolean)
+            End Get
+            Set
+                Me("isNewVersion") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
