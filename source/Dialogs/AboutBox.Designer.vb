2efe542d927f9b6074fd48fdf742fb3bd85cd75b
jamaa <90166+jamaa@users.noreply.github.com>
2025-09-02 22:36:55 +0200

    update list of developers

--- source/Dialogs/AboutBox.Designer.vb
+++ source/Dialogs/AboutBox.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class AboutBox
     Inherits System.Windows.Forms.Form
 
