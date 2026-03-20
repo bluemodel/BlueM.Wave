30b49bdefdce1fdf9c158bd8c04731ca6c35da43
jamaa <90166+jamaa@users.noreply.github.com>
2026-03-20 18:56:47 +0100

    upgrade Wave.Tests to .NET 8

--- tests/My Project/AssemblyInfo.vb
+++ tests/My Project/AssemblyInfo.vb
@@ -1,18 +1,8 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
-
-<Assembly: AssemblyTitle("BlueM.Wave.Tests")>
-<Assembly: AssemblyDescription("Unit testing assembly for BlueM.Wave")>
-<Assembly: AssemblyCompany("BlueM Dev Group")>
-<Assembly: AssemblyProduct("BlueM.Wave.Tests")>
-<Assembly: AssemblyCopyright("Copyright © BlueM Dev Group")>
 <Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 <Assembly: Guid("52f7642c-b8dd-494e-8cbc-bc773c3f6e72")>
-
-' <Assembly: AssemblyVersion("1.0.*")> 
-<Assembly: AssemblyVersion("1.13.4.*")>
-<Assembly: AssemblyFileVersion("1.13.4.0")>
