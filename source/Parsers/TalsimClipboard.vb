e17dd37073d07edcade91db6c61a891496d8b6bc
jamaa <90166+jamaa@users.noreply.github.com>
2026-03-20 17:38:47 +0100

    more robust splitting of lines in clipboard text

--- source/Parsers/TalsimClipboard.vb
+++ source/Parsers/TalsimClipboard.vb
@@ -102,7 +102,7 @@ Namespace Parsers
             Dim file, name As String
 
             zreblock = False
-            For Each line As String In InputText.Split(eol)
+            For Each line As String In InputText.Split(New String() {vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
 
                 line = line.Trim()
 
