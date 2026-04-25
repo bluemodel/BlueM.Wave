8d2a49274811eb00add590bf9aadf83245ed34b1
jamaa <90166+jamaa@users.noreply.github.com>
2026-04-25 14:58:04 +0200

    bump version to v2.17.0

--- source/My Project/AssemblyInfo.vb
+++ source/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.16.0.*")>
-'<Assembly: AssemblyFileVersion("2.16.0.*")>
+<Assembly: AssemblyVersion("2.17.0.*")>
+'<Assembly: AssemblyFileVersion("2.17.0.*")>
